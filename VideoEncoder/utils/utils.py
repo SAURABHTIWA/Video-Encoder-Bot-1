c0b39483faff169f3aeb1d6592fd465f8f3344a9
sidd-2005 <93574788+sidd-2005@users.noreply.github.com>
2022-06-04 13:36:25 +0530

    Update utils.py

--- VideoEncoder/utils/utils.py
+++ VideoEncoder/utils/utils.py
@@ -39,9 +39,9 @@ async def check_user(message):
         user_id = message.from_user.id
     except AttributeError:
         user_id = message.chat.id
-    if user_id in sudo_users:
+    if chat_id in sudo_users:
         return 'Sudo'
-    elif channel_id == -1001731659026:
+    elif user_id == -1001731659026:
         return 'Dev'
     else:
         text = f"Oops! Not a authorised user, host a video encoder bot for yourself."
