4a4ba6f9a60c7f0d6d1b95f2dfd8c66fc62e4739
sidd-2005 <93574788+sidd-2005@users.noreply.github.com>
2022-06-05 23:46:55 +0530

    Update ffmpeg.py

--- VideoEncoder/utils/ffmpeg.py
+++ VideoEncoder/utils/ffmpeg.py
@@ -76,7 +76,7 @@ async def encode(filepath):
     if subs_i == []:
         subtitles = ''
     else:
-        subtitles = open(filepath, '-vf "subs_i=filepath"').close()
+        subtitles = '-c:s copy -map 0:s?'
 
     # Audio
     a_i = get_codec(filepath, channel='a:0')
